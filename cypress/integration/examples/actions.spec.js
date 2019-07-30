025d5a847123573e2ef76ecdd8226ef030a145d9
ExecuteAutomation <kartmcad@gmail.com>
2019-07-30 21:04:47 +1200

    added sceanario

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -40,6 +40,14 @@ context('Actions', () => {
       .prev().should('have.attr', 'style', 'color: orange;')
   })
 
+
+  it('.focus() - focus on a DOM element of UI', () => {
+    // https://on.cypress.io/focus
+    cy.get('.action-focus').focus()
+      .should('have.class', 'focus')
+      .prev().should('have.attr', 'style', 'color: orange;')
+  })
+
   it('.blur() - blur off a DOM element', () => {
     // https://on.cypress.io/blur
     cy.get('.action-blur').type('About to blur').blur()
