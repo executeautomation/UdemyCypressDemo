a87d04e8e992b1405af4ee1db2fb772ec3166565
ExecuteAutomation <kartmcad@gmail.com>
2019-07-31 21:24:22 +1200

    Added docker compose file

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -57,3 +57,14 @@ context('Actions', () => {
 
  
 })
+
+docker run \
+   -u root \
+   -d \
+   --name blue-ocean \
+   -p 8080:8080 \
+   -v jenkins-data:/var/jenkins_home \
+   -v npm-cache:/root/.npm \
+   -v cypress-cache:/root/.cache \
+   -v /var/run/docker.sock:/var/run/docker.sock \
+   jenkinsci/blueocean:latest
